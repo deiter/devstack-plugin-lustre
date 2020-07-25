875e2bea1a6a930091f13a4da2946904aa1c5a39
Alex Deiter <alex.deiter@gmail.com>
2020-07-25 08:43:54 +0000

    Updated Devstack functions

--- devstack/plugin.sh
+++ devstack/plugin.sh
@@ -1,5 +1,14 @@
 # Devstack Lustre plugin
 
+function init_cinder_backend_lustre {
+    local backend="$1"
+
+    if ! mount.lustre -h; then
+        echo 'Required package lustre-client is not installed'
+	exit 1
+    fi
+}
+
 function configure_cinder_backend_lustre {
     local backend="$1"
     local prefix="LUSTRE_OPTGROUP_${backend}_"
@@ -14,6 +23,15 @@ function configure_cinder_backend_lustre {
     done
 }
 
+function cleanup_cinder_backend_lustre {
+    local backend="$1"
+    local mount_base="$DATA_DIR/cinder/mnt"
+
+    if [[ -d "$mount_base" ]]; then
+        mount -t lustre | awk '$3 ~ "^'$mount_base'" {print $3}' | xargs -r -t -L1 sudo umount -l
+    fi
+}
+
 function configure_tempest_lustre {
     iniset $TEMPEST_CONFIG volume-feature-enabled snapshot True
     iniset $TEMPEST_CONFIG volume-feature-enabled backup False
@@ -23,12 +41,14 @@ function configure_tempest_lustre {
 
 if [[ "$1" == "stack" && "$2" == "pre-install" ]]; then
     true
+elif [[ "$1" == "stack" && "$2" == "install" ]]; then
+    true
 elif [[ "$1" == "stack" && "$2" == "post-config" ]]; then
+    true
+elif [[ "$1" == "stack" && "$2" == "extra" ]]; then
     true
 elif [[ "$1" == "stack" && "$2" == "test-config" ]]; then
     configure_tempest_lustre
 elif [[ "$1" == "unstack" ]]; then
-    if [[ -d ${DATA_DIR}/cinder/mnt ]]; then
-        mount -t lustre | awk '{print $3}' | grep "^${DATA_DIR}/" | xargs -r -t -L1 sudo umount -l
-    fi
+    true
 fi
